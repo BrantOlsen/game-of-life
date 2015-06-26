6e51ddbfe69307f10a75cdc4e784244ca00c78cf
Brant Olsen <brantolsen@gmail.com>
2015-06-26 09:39:41 -0400

    Can use either an html element or canvas.

--- game-of-life.js
+++ game-of-life.js
@@ -2,136 +2,158 @@
 
 /**
  * Javascript implementation of Conway's Game of Life.
- * See http://en.wikipedia.org/wiki/Conway's_Game_of_Life for more 
+ * See http://en.wikipedia.org/wiki/Conway's_Game_of_Life for more
  * information.
  */
-function GameOfLife(context, settings)
-{
+function GameOfLife(context, settings) {
   // Force settings to be an object.
   settings = settings || {};
   // Save the settings for use everywhere.
   this.settings = settings;
-    
+
   this.board = {};
+  this.board.div_class = 'game-of-life-div';
   this.board.context = context;
   this.board.cellSize = 25;
-  if (this.settings.cellSize)
-  {
-    this.board.cellSize = this.settings.cellSize;   
+  if (this.settings.cellSize) {
+    this.board.cellSize = this.settings.cellSize;
   }
   this.settings.drawLines = this.settings.drawLines || false;
-  
+
   // Do an initial resize.
   this.resize();
-  
-  if (this.settings.debug) 
-  {
+
+  if (this.settings.debug) {
     console.log("Created a new GameOfLife object.");
   }
-}
- 
+};
+
 /**
   * Determine the number of rows and columns of the board based on the size
   * of the canvas and cellSize.
-  * 
+  *
   * Please note that this will reset boards state.
   */
-GameOfLife.prototype.resize = function() 
-{
-  this.board.rows = Math.floor(this.board.context.canvas.height / this.board.cellSize);
-  if (this.settings.maxRows && this.settings.maxRows > this.board.rows)
-  {
+GameOfLife.prototype.resize = function() {
+  this.board.rows = Math.floor(this.getViewportHeight() / this.board.cellSize);
+  if (this.settings.maxRows && this.settings.maxRows > this.board.rows) {
     this.board.rows = this.settings.maxRows;
   }
-  this.board.columns = Math.floor(this.board.context.canvas.width / this.board.cellSize);
-  if (this.settings.maxColumns && this.settings.maxColumns > this.board.columns)
-  {
+  this.board.columns = Math.floor(this.getViewportWidth() / this.board.cellSize);
+  if (this.settings.maxColumns && this.settings.maxColumns > this.board.columns) {
     this.board.columns = this.settings.maxColumns;
   }
-    
+
   // Init the this.board and its next state with blank values.
   this.board.nextState = new Array(this.board.rows);
-  for (var i = 0; i < this.board.rows; ++i)
-  {
+  for (var i = 0; i < this.board.rows; ++i) {
     this.board[i] = new Array(this.board.columns);
     this.board.nextState[i] = new Array(this.board.columns);
-    for (var j = 0; j < this.board.columns; ++j)
-    {
+    for (var j = 0; j < this.board.columns; ++j) {
       this.board[i][j] = 0;
       this.board.nextState[i][j] = 0;
     }
   }
-  
-  if (this.settings.debug) 
-  {
+
+  if (this.settings.debug) {
     console.log("Resized the board.");
   }
-}
-  
+};
+
+/**
+ * Get the height of the container that the gameboard
+ * is contained within.
+ */
+GameOfLife.prototype.getViewportHeight = function() {
+  if (this.board.context.canvas != null) {
+    return this.board.context.canvas.height;
+  }
+  else {
+    return $(this.board.context).height();
+  }
+};
+
+/**
+ * Get the width of the container that the gameboard
+ * is contained within.
+ */
+GameOfLife.prototype.getViewportWidth = function() {
+  if (this.board.context.canvas != null) {
+    return this.board.context.canvas.width;
+  }
+  else {
+    return $(this.board.context).width();
+  }
+};
+
 /**
  * Draw an orange line from the start coordinates to end coordinates.
  */
-GameOfLife.prototype.drawLine = function(xStart, yStart, xEnd, yEnd)
-{
-  this.board.context.fillStyle = 'orange';
-  this.board.context.beginPath();
-  this.board.context.moveTo(xStart, yStart);
-  this.board.context.lineTo(xEnd, yEnd);
-  this.board.context.closePath();
-  this.board.context.stroke();
-}
-  
+GameOfLife.prototype.drawLine = function(xStart, yStart, xEnd, yEnd) {
+  if (this.board.context.fillStyle) {
+    this.board.context.fillStyle = 'orange';
+    this.board.context.beginPath();
+    this.board.context.moveTo(xStart, yStart);
+    this.board.context.lineTo(xEnd, yEnd);
+    this.board.context.closePath();
+    this.board.context.stroke();
+  }
+};
+
 /**
   * Fill the given cell for the row and column with a black box.
   *
   * @param row The row of the cell.
   * @column column The column of the cell.
   */
-GameOfLife.prototype.fillCell = function(row, column) 
-{
-  if (this.settings.color)
-  {
-    this.board.context.fillStyle = this.settings.color;
-  }
-  else
-  {
-    this.board.context.fillStyle = "black";
-  }
-   
+GameOfLife.prototype.fillCell = function(row, column) {
+  this.settings.color = this.settings.color || "black";
   var x = column * this.board.cellSize;
   var y = row * this.board.cellSize;
-  this.board.context.fillRect(x, y, this.board.cellSize, this.board.cellSize);
-}
+
+  if (this.board.context.fillStyle) {
+    this.board.context.fillStyle = this.settings.color;
+    this.board.context.fillRect(x, y, this.board.cellSize, this.board.cellSize);
+  }
+  else {
+    var rect_div = $('<div></div>');
+    rect_div.css('position', 'absolute')
+      .addClass('game-of-life-div')
+      .css('bottom', y)
+      .css('left', x)
+      .css('z-index', 7)
+      .css('width', this.board.cellSize)
+      .css('height', this.board.cellSize)
+      .css('background-color', this.settings.color)
+      .css('boarder', '1px solid black');
+    this.board.context.append(rect_div);
+  }
+};
 
 /**
   * Fill all cells that contain the value 1.
   */
-GameOfLife.prototype.fillCells = function() 
-{
-  for (var i = 0; i < this.board.rows; ++i)
-  {
-    if (this.settings.drawLines)
-    {
-      this.drawLine(0, i*this.board.cellSize, this.board.context.canvas.width, i*this.board.cellSize);
+GameOfLife.prototype.fillCells = function() {
+  for (var i = 0; i < this.board.rows; ++i) {
+    if (this.settings.drawLines) {
+      this.drawLine(0, i*this.board.cellSize, this.getViewportWidth(), i*this.board.cellSize);
     }
-    for (var j = 0; j < this.board.columns; ++j)
-    {
-      if (this.board[i][j] == 1)
-      {
+
+    for (var j = 0; j < this.board.columns; ++j) {
+      if (this.board[i][j] == 1) {
         this.fillCell(i, j);
       }
-      if (this.settings.drawLines && i == 0)
-      {
-        this.drawLine(j*this.board.cellSize, 0, j*this.board.cellSize, this.board.context.canvas.height);
+
+      if (this.settings.drawLines && i == 0) {
+        this.drawLine(j*this.board.cellSize, 0, j*this.board.cellSize, this.getViewportHeight());
       }
     }
   }
-  
-  if (this.settings.debug) 
-  {
+
+  if (this.settings.debug) {
     console.log("fillCells.");
   }
-}
+};
 
 /**
   * Determine how many adjacent neighbors are alive for this cell.
@@ -236,15 +258,18 @@ GameOfLife.prototype.moveToNextState = function()
 /**
   * Clear the canvas of everything.
   */
-GameOfLife.prototype.clear = function()
-{
-  this.board.context.clearRect(0, 0, this.board.context.canvas.width, this.board.context.canvas.height);
-  
-  if (this.settings.debug) 
-  {
+GameOfLife.prototype.clear = function() {
+  if (this.board.context.clearRect) {
+    this.board.context.clearRect(0, 0, this.getViewportWidth(), this.getViewportHeight());
+  }
+  else {
+    this.board.context.find('div.game-of-life-div').remove();
+  }
+
+  if (this.settings.debug) {
     console.log("Clear the board.");
   }
-}
+};
 
 /**
   * Draw the this.board. Then update it for the next call to update.
@@ -255,8 +280,8 @@ GameOfLife.prototype.update = function()
   this.fillCells();
   this.determineNextState();
   this.moveToNextState();
-  
-  if (this.settings.debug) 
+
+  if (this.settings.debug)
   {
     console.log("updating the game of life.");
   }
@@ -267,41 +292,41 @@ GameOfLife.prototype.update = function()
   * on the upateInterval given by the user, or half a second if none
   * was given.
   */
-GameOfLife.prototype.start = function() 
+GameOfLife.prototype.start = function()
 {
   var self = this;
-    
+
   var updateInterval = 500;
   if (this.settings.updateInterval)
   {
     updateInterval = this.settings.updateInterval;
   }
-  
+
   this.intervalID = setInterval(function() {
     self.update();
   }, updateInterval);
-  
-  if (this.settings.debug) 
+
+  if (this.settings.debug)
   {
     console.log("Start the game of life.");
   }
 }
-  
+
 /**
   * Clear the setInterval used in the start function. This will halt the board
   * updating.
   */
-GameOfLife.prototype.stop = function () 
+GameOfLife.prototype.stop = function ()
 {
   clearInterval(this.intervalID);
   this.intervalID = undefined;
-  
-  if (this.settings.debug) 
+
+  if (this.settings.debug)
   {
     console.log("Stop the game of life.");
   }
 }
-  
+
 /**
   * Toggle between start and stop based on whether or not we
   * know the interval id.
@@ -320,28 +345,28 @@ GameOfLife.prototype.toggle = function () {
 /**
  * Check if the given parameters will fit in the current board's
  * size.
- * 
+ *
  * @return True if the arguments will fit.
  */
 GameOfLife.prototype.canFit = function(row, column, rowStretch, colStretch)
 {
-  if (row+rowStretch > this.board.rows || 
+  if (row+rowStretch > this.board.rows ||
       row < 0 ||
       column+colStretch > this.board.columns ||
       column < 0)
   {
     return false;
   }
-  
+
   return true;
 }
 
 /**
   * Create a small glider at the given coords.
-  * 
+  *
   * @param row The row to start the glider at.
   * @param column The column to start the glider at.
-  * 
+  *
   * @return True if the glider was created.
   */
 GameOfLife.prototype.createGlider = function(row, column)
@@ -351,27 +376,27 @@ GameOfLife.prototype.createGlider = function(row, column)
   {
     return false;
   }
-    
+
   this.board[row+1][column] = 1;
   this.board[row+2][column+1] = 1;
   this.board[row+2][column+2] = 1;
   this.board[row+1][column+2] = 1;
   this.board[row][column+2] = 1;
-    
-  if (this.settings.debug) 
+
+  if (this.settings.debug)
   {
     console.log("Create a glider at " + row + ", " + column);
   }
-    
+
   return true;
 }
-  
+
 /**
   * Create a small glider at the given coords.
-  * 
+  *
   * @param row The row to start the glider at.
   * @param column The column to start the glider at.
-  * 
+  *
   * @return True if the glider was created.
   */
 GameOfLife.prototype.createAcorn = function(row, column)
@@ -381,7 +406,7 @@ GameOfLife.prototype.createAcorn = function(row, column)
   {
     return false;
   }
-    
+
   this.board[row+3][column] = 1;
   this.board[row+1][column+1] = 1;
   this.board[row+3][column+1] = 1;
@@ -389,21 +414,21 @@ GameOfLife.prototype.createAcorn = function(row, column)
   this.board[row+3][column+4] = 1;
   this.board[row+3][column+5] = 1;
   this.board[row+3][column+6] = 1;
-    
-  if (this.settings.debug) 
+
+  if (this.settings.debug)
   {
     console.log("Create a glider at " + row + ", " + column);
   }
-    
+
   return true;
 }
 
 /**
   * Create a small glider at the given coords.
-  * 
+  *
   * @param row The row to start the glider at.
   * @param column The column to start the glider at.
-  * 
+  *
   * @return True if the glider was created.
   */
 GameOfLife.prototype.createBeacon = function(row, column)
@@ -413,7 +438,7 @@ GameOfLife.prototype.createBeacon = function(row, column)
   {
     return false;
   }
-    
+
   this.board[row][column] = 1;
   this.board[row+1][column] = 1;
   this.board[row][column+1] = 1;
@@ -422,21 +447,21 @@ GameOfLife.prototype.createBeacon = function(row, column)
   this.board[row+3][column+2] = 1;
   this.board[row+2][column+3] = 1;
   this.board[row+3][column+3] = 1;
-    
-  if (this.settings.debug) 
+
+  if (this.settings.debug)
   {
     console.log("Create a glider at " + row + ", " + column);
   }
-    
+
   return true;
 }
-  
+
 /**
   * Create a small glider gun at the given coords.
-  * 
+  *
   * @param row The row to start the gun at.
   * @param column The column to start the gun at.
-  * 
+  *
   * @return True if the gun was created.
   */
 GameOfLife.prototype.createGliderGun = function(row, column)
@@ -446,13 +471,13 @@ GameOfLife.prototype.createGliderGun = function(row, column)
   {
     return false;
   }
-  
-  // Left square.  
+
+  // Left square.
   this.board[row+4][column] = 1;
   this.board[row+4][column+1] = 1;
   this.board[row+5][column] = 1;
   this.board[row+5][column+1] = 1;
-  
+
   // Middle almost circle.
   this.board[row+4][column+10] = 1;
   this.board[row+5][column+10] = 1;
@@ -470,7 +495,7 @@ GameOfLife.prototype.createGliderGun = function(row, column)
   this.board[row+6][column+16] = 1;
   this.board[row+7][column+15] = 1;
   this.board[row+5][column+17] = 1;
-  
+
   // Arrow
   this.board[row+2][column+20] = 1;
   this.board[row+3][column+20] = 1;
@@ -484,14 +509,14 @@ GameOfLife.prototype.createGliderGun = function(row, column)
   this.board[row+1][column+24] = 1;
   this.board[row+5][column+24] = 1;
   this.board[row+6][column+24] = 1;
- 
+
   // Right square
   this.board[row+2][column+34] = 1;
   this.board[row+2][column+35] = 1;
   this.board[row+3][column+34] = 1;
   this.board[row+3][column+35] = 1;
 
-  if (this.settings.debug) 
+  if (this.settings.debug)
   {
     console.log("Create glider gun at " + row + ", " + column);
   }
@@ -508,13 +533,13 @@ GameOfLife.prototype.createF_Pentomino = function(row, column)
   {
     return false;
   }
-  
+
   this.board[row+1][column] = 1;
   this.board[row][column+1] = 1;
   this.board[row+1][column+1] = 1;
   this.board[row+2][column+1] = 1;
   this.board[row][column+2] = 1;
-  
+
   return true;
 }
 
@@ -527,7 +552,7 @@ GameOfLife.prototype.create1RowGun = function(row, column)
   {
     return false;
   }
-  
+
   this.board[row][column] = 1;
   this.board[row][column+1] = 1;
   this.board[row][column+2] = 1;
@@ -536,17 +561,17 @@ GameOfLife.prototype.create1RowGun = function(row, column)
   this.board[row][column+5] = 1;
   this.board[row][column+6] = 1;
   this.board[row][column+7] = 1;
-  
+
   this.board[row][column+9] = 1;
   this.board[row][column+10] = 1;
   this.board[row][column+11] = 1;
   this.board[row][column+12] = 1;
   this.board[row][column+13] = 1;
-  
+
   this.board[row][column+17] = 1;
   this.board[row][column+18] = 1;
   this.board[row][column+19] = 1;
-  
+
   this.board[row][column+26] = 1;
   this.board[row][column+27] = 1;
   this.board[row][column+28] = 1;
@@ -554,35 +579,47 @@ GameOfLife.prototype.create1RowGun = function(row, column)
   this.board[row][column+30] = 1;
   this.board[row][column+31] = 1;
   this.board[row][column+32] = 1;
-  
+
   this.board[row][column+34] = 1;
   this.board[row][column+35] = 1;
   this.board[row][column+36] = 1;
   this.board[row][column+37] = 1;
   this.board[row][column+38] = 1;
-  
+
   return true;
 }
 
 /**
  * Add a cell based on the x and y coordinates of the click. This
  * will also call fillCells.
- * 
+ *
  * @param clickEvent The click event to obtain x and y coords from.
  */
-GameOfLife.prototype.fillByClick = function(clickEvent)
-{
+GameOfLife.prototype.fillByClick = function(clickEvent) {
   var x = clickEvent.clientX;
   var y = clickEvent.clientY;
-  
+
   var col = Math.floor(x / this.board.cellSize);
   var row = Math.floor(y / this.board.cellSize);
-  
+
   this.board[row][col] = 1;
   this.fillCells();
-  
-  if (this.settings.debug) 
+
+  if (this.settings.debug)
   {
     console.log("Fill by click on " + x + ", " + y);
   }
 }
+
+GameOfLife.prototype.destroyByClick = function(click_event) {
+  var x = clickEvent.clientX;
+  var y = clickEvent.clientY;
+
+  var col = Math.floor(x / this.board.cellSize);
+  var row = Math.floor(y / this.board.cellSize);
+
+  if (this.board[row][col] == 1) {
+    this.board[row][col] = 0;
+  }  
+  this.fillCells();
+};
